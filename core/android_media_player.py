cf5bda921d7e2397d6bfa4fcb9f652c65539a4e0
Jean Abraham Chacón Candanosa <jeanabrahamchc@gmail.com>
2026-06-20 23:00:25 -0600

    new logic

--- core/android_media_player.py
+++ core/android_media_player.py
@@ -1,7 +1,9 @@
-from jnius import autoclass
+from jnius import autoclass, cast
 from core.android_sound_info import get_audio_length
 
 MediaPlayer = autoclass("android.media.MediaPlayer")
+PythonActivity = autoclass('org.kivy.android.PythonActivity')
+Uri = autoclass('android.net.Uri')
 
 class AndroidMediaPlayer:
     '''
@@ -9,11 +11,39 @@ class AndroidMediaPlayer:
     '''
     def __init__(self, source):
         self._media_player = None
+        self._source = source
 
         try:
             self._media_player = MediaPlayer()
 
-            self._media_player.setDataSource(str(source))
+            source_str = str(source)
+
+            # Si es una URL de internet o streaming, se setea directo por texto
+            if source_str.startswith("http://") or source_str.startswith("https://") or source_str.startswith("rtsp://"):
+                self._media_player.setDataSource(source_str)
+            else:
+                # Es un archivo local o un Content URI del selector nativo.
+                # Aplicamos el truco del FileDescriptor para saltar Scoped Storage.
+                activity = PythonActivity.mActivity
+                context = activity.getApplicationContext()
+                content_resolver = context.getContentResolver()
+
+                if source_str.startswith('content://'):
+                    file_uri = Uri.parse(source_str)
+                else:
+                    # Si es una ruta absoluta limpia de Linux (/storage/emulated/0...)
+                    File = autoclass('java.io.File')
+                    file_uri = Uri.fromFile(File(source_str))
+
+                # Abrimos el canal nativo como modo lectura ("r")
+                pfd = content_resolver.openFileDescriptor(file_uri, "r")
+                fd = pfd.getFileDescriptor()
+
+                # Le alimentamos el descriptor directo a la instancia
+                self._media_player.setDataSource(fd)
+                pfd.close() # Se cierra el puente, MediaPlayer ya retiene el archivo en RAM nativa
+
+            # Una vez asignado el origen, preparamos síncronamente
             self._media_player.prepare()
 
         except Exception as e:
@@ -23,7 +53,6 @@ class AndroidMediaPlayer:
                     self._media_player.release()
                 except:
                     pass
-
             self._media_player = None
 
     def is_valid(self) -> bool:
